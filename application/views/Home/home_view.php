709f4c3100b20520702cad05bf21cdc22eab940f
OrtegaF <ortegavaldesf@gmail.com>
2023-10-30 21:58:40 -0600

    Primeros avances del home

---
+++ application/views/Home/home_view.php
@@ -0,0 +1,47 @@
+<section class="content">
+    <div class="row">
+        <!--<div class="col-lg-4">
+            <div class="card card-warning">
+                <div class="card-header">
+                    <h3 class="card-title"></h3>
+                </div>
+                <div class="card-body">
+
+                </div>
+                <div class="card-footer">
+
+                </div>
+            </div>
+        </div>-->
+        <div class="col-lg-4">
+            <div class="card" style="width: 18rem;">
+                <img src="./vendor/dist/img/photo1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card" style="width: 18rem;">
+                <img src="./vendor/dist/img/photo2.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card" style="width: 18rem;">
+                <img src="./vendor/dist/img/photo3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
