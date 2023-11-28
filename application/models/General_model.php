c23d37e0600ecf06217cd54a28baa14be5fe33e7
Abigail6B <utp0145723@alumno.utpuebla.edu.mx>
2023-11-28 12:00:16 -0600

    Actualizaciones 28Nov

--- application/models/General_model.php
+++ application/models/General_model.php
@@ -35,59 +35,6 @@ class General_model extends CI_Model
 			return false;
 	}
 
-	public function get_or($tabla='', $atributos, $orden, $agrupamiento='')
-	{
-		if ($agrupamiento!='') {
-			$this->db->group_by($agrupamiento);
-		}
-	    if(count($orden)>0){
-	        foreach ($orden as $cve => $val){
-	            $this->db->order_by($cve, $val);
-	        }
-		}
-		if(count($atributos)>0){
-			$contador = 0;
-			foreach ($atributos as $cve => $val)
-				if ($contador<1)
-					$this->db->where($cve, $val);
-				else
-					$this->db->or_where($cve, $val);
-				$contador++;
-		}
-		$resultado = $this->db->get($tabla);
-		if ($resultado->num_rows()>0){
-			return $resultado;
-		}
-		else
-			return false;
-	}
-
-	public function get_like($tabla='', $atributos, $orden, $agrupamiento='', $like)
-	{
-		if ($agrupamiento!='') {
-			$this->db->group_by($agrupamiento);
-		}
-	    if(count($orden)>0){
-	        foreach ($orden as $cve => $val){
-	            $this->db->order_by($cve, $val);
-	        }
-		}
-		if(count($atributos)>0){
-			foreach ($atributos as $cve => $val)
-				$this->db->where($cve, $val);
-		}
-		if (count($like)>0) {
-			foreach ($like as $cve => $val)
-				$this->db->like($cve, $val);
-		}
-		$resultado = $this->db->get($tabla);
-		if ($resultado->num_rows()>0){
-			return $resultado;
-		}
-		else
-			return false;
-	}
-
 	public function set($tabla, $valores)
 	{
 		$this->db->insert($tabla, $valores);
@@ -118,17 +65,6 @@ class General_model extends CI_Model
 		$this->db->query($sql);
 	}
 
-	public function index($tabla, $cve)
-	{
-		$sql = "SELECT MAX($cve) as 'indice' from $tabla";
-		$resultado = $this->db->query($sql);
-		if ($resultado->num_rows()>0) {
-			$res = $resultado->row(0);
-			return $res->indice;
-		}
-		else
-			return false;
-	}
 
 	public function delete($tabla='', $atributos)
 	{
@@ -139,26 +75,5 @@ class General_model extends CI_Model
 		$this->db->delete($tabla);
 	}
 
-	public function query($sql){
-		$resultado = $this->db->query($sql);
-		if ($resultado->num_rows()>0) {
-			return $resultado;
-		}
-		else
-			return false;
-	}
-
-	public function truncar($tabla){
-		$this->db->truncate($tabla);
-	}
-
-	public function fields($tabla=''){
-		$fields = $this->db->list_fields($tabla);
-		if (count($fields)>0){
-			return $fields;
-		}
-		else
-			return false;
-	}
 
 }
